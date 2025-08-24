afe0e56722f618a629c012ba398f64d55a973265
Sapferum <stoeglazov90@gmail.com>
2025-08-24 12:28:56 +0200

    resolve-conflict

--- src/firebase.client.js
+++ src/firebase.client.js
@@ -1,12 +1,42 @@
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, setPersistence, browserLocalPersistence } from 'firebase/auth';
-// import { getFirestore, updateDoc, deleteField, deleteDoc } from 'firebase/firestore';
-// import { useFirebase } from 'vuefire'
-//
-//
-// const { $firebase } = useFirebase()
-// const auth = getAuth($firebase);
-// const db = getFirestore($firebase);
-//
-// setPersistence(auth, browserLocalPersistence);
-//
-// export { auth, db, deleteField, deleteDoc, updateDoc, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+// plugins/firebase.client.js
+import { initializeApp } from 'firebase/app'
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    setPersistence,
+    browserLocalPersistence
+} from 'firebase/auth'
+import {
+    getFirestore,
+    updateDoc,
+    deleteField,
+    deleteDoc
+} from 'firebase/firestore'
+
+import { firebaseConfig } from '../config/firebaseConfig.js'
+
+export default defineNuxtPlugin(() => {
+    // инициализация Firebase
+    const app = initializeApp(firebaseConfig)
+
+    const auth = getAuth(app)
+    const db = getFirestore(app)
+
+    setPersistence(auth, browserLocalPersistence)
+
+    // пробрасываем в nuxtApp.$firebase
+    return {
+        provide: {
+            firebase: {
+                auth,
+                db,
+                deleteField,
+                deleteDoc,
+                updateDoc,
+                createUserWithEmailAndPassword,
+                signInWithEmailAndPassword
+            }
+        }
+    }
+})
