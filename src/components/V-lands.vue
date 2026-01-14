86d007ed0a771de6575819a13775bfda191e3de5
Igor <igor.chudesny1991@gmail.com>
2026-01-14 21:44:53 +0200

    review new region

--- src/components/V-lands.vue
+++ src/components/V-lands.vue
@@ -89,7 +89,6 @@ import {ref, computed, onMounted, onBeforeUnmount, watch} from 'vue'
 import {useRouter} from 'vue-router'
 import {regions} from '@/utils/regions.js'
 import {userlangStore} from '../../store/learningStore.js'
-
 const {t, locale} = useI18n()
 const langStore = userlangStore()
 const router = useRouter()
