69b733e732ed73d394d21df81a58e14d8bc442f6
Igor <igor.chudesny1991@gmail.com>
2026-01-30 15:02:15 +0200

    fix color title

--- pages/description-images-test.vue
+++ pages/description-images-test.vue
@@ -263,7 +263,7 @@ async function sendMessage(voiceText = null) {
       <header class="app-header">
         <button @click="goBack" class="btn-icon-back">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+               stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <line x1="19" y1="12" x2="5" y2="12"></line>
             <polyline points="12 19 5 12 12 5"></polyline>
           </svg>
@@ -447,7 +447,7 @@ async function sendMessage(voiceText = null) {
 }
 
 .header-title {
-  font-size: 1.6rem;
+  font-size: 1.1rem;
   font-weight: 700;
   color: var(--titleColor);
 }
