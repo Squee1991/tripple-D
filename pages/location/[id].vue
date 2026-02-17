2d1d2811e559e04914a73c34cff1fa6665a456d5
Igor <igor.chudesny1991@gmail.com>
2026-02-17 22:51:26 +0100

    remove regions

--- pages/location/[id].vue
+++ pages/location/[id].vue
@@ -266,7 +266,7 @@ onMounted(async () => {
   padding: 18px 16px 16px;
   box-shadow: 4px 4px 0 #2b2b2b;
   transform: translateY(0) rotate(-.1deg);
-  transition: all .1s ease-in-out;
+  will-change: transform, opacity;
   opacity: 0;
   animation: pop .45s cubic-bezier(.2, .8, .25, 1) forwards;
 }
