8734f448c0a71b6b901308e0d8c7d3bc23ebc195
Igor <igor.chudesny1991@gmail.com>
2026-02-19 21:26:43 +0100

    fix level  mistake in regions

--- utils/regions.js
+++ utils/regions.js
@@ -70,7 +70,7 @@ export const regions = {
 				pathTo: "master-lands",
 				theme: "master",
 				name: "locationNames.master-lands",
-				level: 0,
+				level: 13,
 				desc: "regions.descriptionMasterLands",
 				icon: MasterLands
 			},
