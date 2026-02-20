5a5e5348a0255e0a99695a3a47365436c0314e74
Igor <igor.chudesny1991@gmail.com>
2026-02-20 18:03:01 +0100

    fix mistakes

--- utils/regions.js
+++ utils/regions.js
@@ -26,7 +26,7 @@ export const regions = {
 				pathTo: "stone-reach",
 				theme: "stone",
 				name: "locationNames.stone-reach",
-				level: 0,
+				level: 3,
 				desc: "regions.descriptionStoneReach",
 				icon: StoneReach
 			},
@@ -35,7 +35,7 @@ export const regions = {
 				pathTo: "wind-valley",
 				theme: "winds",
 				name: "locationNames.wind-valley",
-				level: 0,
+				level: 5,
 				desc: "regions.descriptionWindValley",
 				icon: WindValley
 			},
@@ -44,7 +44,7 @@ export const regions = {
 				pathTo: "lake-ruins",
 				theme: "ruins",
 				name: "locationNames.lake-ruins",
-				level: 0,
+				level: 7,
 				desc: "regions.descriptionLakeRuins",
 				icon: LakeRuins
 			},
@@ -53,7 +53,7 @@ export const regions = {
 				pathTo: "north-cliffs",
 				theme: "north",
 				name: "locationNames.north-cliffs",
-				level: 0,
+				level: 9,
 				desc: "regions.descriptionNorthCliffs",
 				icon: NorthCliffs
 			},
@@ -62,7 +62,7 @@ export const regions = {
 				pathTo: "storm-coast",
 				theme: "storm",
 				name: "locationNames.storm-coast",
-				level: 0,
+				level: 11,
 				desc: "regions.descriptionStormCoast",
 				icon: StormCoast
 			},
@@ -71,7 +71,7 @@ export const regions = {
 				pathTo: "master-lands",
 				theme: "master",
 				name: "locationNames.master-lands",
-				level: 0,
+				level: 13,
 				desc: "regions.descriptionMasterLands",
 				icon: MasterLands
 			},
@@ -80,38 +80,38 @@ export const regions = {
 				pathTo: "wild-lands",
 				theme: "wild",
 				name: "locationNames.wild-lands",
-				level: 0,
+				level: 15,
 				desc: "regions.descriptionWildLands",
 				icon: WildLand
 			}
 		],
 		intermediate: [
-			{
-				id: "plural-land",
-				pathTo: "plural-land",
-				theme: "plural",
-				name: "Перевал грядущего",
-				level: 0,
-				desc: "На этом крутом склоне туман прошлого окончательно рассеивается, открывая вид на далекий горизонт. Здесь путник учится не просто смотреть под ноги, а заглядывать в завтрашний и прошедший день",
-				icon: Future
-			},
-			{
-				id: "past-land",
-				pathTo: "past-land",
-				theme: "past",
-				name: "Руины былого",
-				level: 0,
-				desc: "Среди древних камней и песков времени хранятся истории о том, что уже свершилось. Здесь путник учится оглядываться назад и описывать события минувших дней, осваивая Прошедшее время.",
-				icon: Past
-			},
-			{
-				id: "action-valley",
-				pathTo: "action-valley",
-				theme: "action",
-				name: "Долина действий",
-				level: 0,
-				desc: "Здесь время больше не стоит на месте, а туман прошлого рассеивается. В этой долине путник учится ставить цели, осваивая модальные глаголы и искусство взаимодействия через падежи.",
-				icon: Action
-			}
+			// {
+			// 	id: "plural-land",
+			// 	pathTo: "plural-land",
+			// 	theme: "plural",
+			// 	name: "Перевал грядущего",
+			// 	level: 0,
+			// 	desc: "На этом крутом склоне туман прошлого окончательно рассеивается, открывая вид на далекий горизонт. Здесь путник учится не просто смотреть под ноги, а заглядывать в завтрашний и прошедший день",
+			// 	icon: Future
+			// },
+			// {
+			// 	id: "past-land",
+			// 	pathTo: "past-land",
+			// 	theme: "past",
+			// 	name: "Руины былого",
+			// 	level: 0,
+			// 	desc: "Среди древних камней и песков времени хранятся истории о том, что уже свершилось. Здесь путник учится оглядываться назад и описывать события минувших дней, осваивая Прошедшее время.",
+			// 	icon: Past
+			// },
+			// {
+			// 	id: "action-valley",
+			// 	pathTo: "action-valley",
+			// 	theme: "action",
+			// 	name: "Долина действий",
+			// 	level: 0,
+			// 	desc: "Здесь время больше не стоит на месте, а туман прошлого рассеивается. В этой долине путник учится ставить цели, осваивая модальные глаголы и искусство взаимодействия через падежи.",
+			// 	icon: Action
+			// }
 		]
 	};
