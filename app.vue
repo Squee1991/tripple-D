e5fe2f537a29976ea6907fb41116834bbc708be2
Igor <igor.chudesny1991@gmail.com>
2026-02-28 02:16:06 +0100

    optimization fetch request

--- app.vue
+++ app.vue
@@ -83,10 +83,12 @@ onMounted(() => {
 
 onMounted(async () => {
   await learningStore.loadFromFirebase()
-  sentencesStore.loadSentences()
-  questStore.loadDailyProgress()
-  cardStore.loadCreatedCount()
-  statsStore.loadLocalStats()
+  // sentencesStore.loadSentences()
+  if (authStore.uid) {
+    questStore.loadDailyProgress()
+    cardStore.loadCreatedCount()
+    statsStore.loadLocalStats()
+  }
   setTimeout(() => {
     if (!achStore.showPopup && !showStepHint.value) {
       onToastFinished()
