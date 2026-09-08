32c2151f7739c10a96abf2f0029b34187c69d66b
Squee1991 <igor.chudesny1991@gmail.com>
2026-09-08 13:07:23 +0200

    new theme

--- pages/recipes/[id]/task.vue
+++ pages/recipes/[id]/task.vue
@@ -6,7 +6,8 @@
         @cancel="cancelExit"
     />
     <div class="dialogue-scene__chef-container">
-      <img :src="chefImage" alt="Chef" class="dialogue-scene__chef-img"/>
+      <img :src="chefImage" alt="Chef"
+           class="dialogue-scene__chef-img"/>
     </div>
     <div class="dialogue-scene__main-content">
       <div class="dialogue-scene__header">
